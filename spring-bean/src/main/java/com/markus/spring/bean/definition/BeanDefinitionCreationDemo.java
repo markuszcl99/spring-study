1f6aef470ea319ef80e878eae0e5a9cf0a816e0b
markuszcl99 <e_markuszhang@163.com>
2022-03-12 23:55:40 +0800

    spring-bean 相关 定义BeanDefinition相关代码填充使其注册到上下文中

--- spring-bean/src/main/java/com/markus/spring/bean/definition/BeanDefinitionCreationDemo.java
+++ spring-bean/src/main/java/com/markus/spring/bean/definition/BeanDefinitionCreationDemo.java
@@ -2,9 +2,15 @@ package com.markus.spring.bean.definition;
 
 import com.markus.spring.ioc.container.domain.User;
 import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 /**
  * @author: markus
@@ -14,6 +20,9 @@ import org.springframework.beans.factory.support.GenericBeanDefinition;
  */
 public class BeanDefinitionCreationDemo {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.refresh();
+
         // 1. 通过BeanDefinitionBuilder
         BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
         beanDefinitionBuilder
@@ -22,6 +31,7 @@ public class BeanDefinitionCreationDemo {
         // 获取Bean实例
         BeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
         // BeanDefinition并非Bean终态，可以自定义修改
+        applicationContext.registerBeanDefinition("user1",beanDefinition);
 
         // 2. 通过AbstractBeanDefinition以及派生类
         GenericBeanDefinition genericBeanDefinition = new GenericBeanDefinition();
@@ -33,5 +43,10 @@ public class BeanDefinitionCreationDemo {
                 .add("age",23)
                 .add("name","Markus");
         genericBeanDefinition.setPropertyValues(mutablePropertyValues);
+        applicationContext.registerBeanDefinition("user2",genericBeanDefinition);
+
+        Map<String,User> users = applicationContext.getBeansOfType(User.class);
+        System.out.println(users);
+        applicationContext.close();
     }
 }
