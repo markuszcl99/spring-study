9d5ccf9949ac1fcdf09a2888324733394ee1a58f
markuszcl99 <e_markuszhang@163.com>
2022-03-25 23:18:42 +0800

    spring 依赖来源 依赖注入与依赖查找的区别

---
+++ spring-dependency-source/src/main/java/com/markus/spring/spring/dependency/source/DependencySourceDemo.java
@@ -0,0 +1,64 @@
+package com.markus.spring.spring.dependency.source;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.ResourceLoader;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author: markus
+ * @date: 2022/3/25 9:32 下午
+ * @Description: 依赖来源 示例
+ * @Blog: http://markuszhang.com/
+ */
+public class DependencySourceDemo {
+    // 注入在 postProcessProperties 中进行，早于 setter注入 @PostConstruct
+    @Autowired
+    private BeanFactory beanFactory;
+    @Autowired
+    private ResourceLoader resourceLoader;
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @PostConstruct
+    public void initByInjection(){
+        System.out.println("beanFactory == applicationContext " + (beanFactory == applicationContext));
+        System.out.println("beanFactory == applicationContext.getBeanFactory() " + (beanFactory == applicationContext.getAutowireCapableBeanFactory()));
+        System.out.println("resourceLoader == applicationContext " + (resourceLoader == applicationContext));
+        System.out.println("applicationEventPublisher == applicationContext " + (applicationEventPublisher == applicationContext));
+    }
+
+    @PostConstruct
+    public void initByLookup(){
+        getBean(BeanFactory.class);
+        getBean(ResourceLoader.class);
+        getBean(ApplicationContext.class);
+        getBean(ApplicationEventPublisher.class);
+    }
+
+    private <T> T getBean(Class<T> beanType){
+        try {
+            return beanFactory.getBean(beanType);
+        }catch (NoSuchBeanDefinitionException e){
+            System.err.println("当前类型" + beanType.getName() + " 无法在 BeanFactory 中查找!");
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(DependencySourceDemo.class);
+
+        applicationContext.refresh();
+
+        applicationContext.close();
+    }
+}
