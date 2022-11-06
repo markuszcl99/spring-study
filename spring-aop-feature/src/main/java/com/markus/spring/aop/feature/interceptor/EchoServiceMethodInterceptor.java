2182df80695e9aa8ecd9bc3bcf842121a15342b9
markuszcl99 <e_markuszhang@163.com>
2022-11-06 12:40:45 +0800

    JoinPoint -> Invocation -> MethodInvocation -> ProxyMethodInvocation -> ReflectiveMethodInvocation -> CglibMethodInvocation 相关的接口了解

--- spring-aop-feature/src/main/java/com/markus/spring/aop/feature/interceptor/EchoServiceMethodInterceptor.java
+++ spring-aop-feature/src/main/java/com/markus/spring/aop/feature/interceptor/EchoServiceMethodInterceptor.java
@@ -2,6 +2,7 @@ package com.markus.spring.aop.feature.interceptor;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.ProxyMethodInvocation;
 
 import java.lang.reflect.Method;
 
@@ -18,6 +19,13 @@ public class EchoServiceMethodInterceptor implements MethodInterceptor {
     public Object invoke(MethodInvocation invocation) throws Throwable {
         Method method = invocation.getMethod();
         System.out.printf("proxy target class is %s, target method is %s \n", method.getDeclaringClass(), method);
+        System.out.printf("Joinpoint#getStaticPart is %s\n", invocation.getStaticPart());
+        System.out.printf("Joinpoint#getThis is %s\n", invocation.getThis());
+        /*这里我知道被拦截方法的参数只有一个*/
+        System.out.printf("Invocation#getArguments is %s\n", invocation.getArguments()[0]);
+        /*这里我想改变方法的参数*/
+        ProxyMethodInvocation methodInvocation = (ProxyMethodInvocation) invocation;
+        methodInvocation.setArguments("update info Hello World!");
         Object result = invocation.proceed();
         return result;
     }
